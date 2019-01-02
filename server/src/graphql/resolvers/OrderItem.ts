2e027e482406489a9de341856fbcb0bbc64e75be
nursh <nsheikhmohammed@yahoo.com>
2019-01-02 01:37:12 -0500

    Created order resolver to handle items relations

--- server/src/graphql/resolvers/OrderItem.ts
+++ server/src/graphql/resolvers/OrderItem.ts
@@ -7,8 +7,10 @@ export default {
   order: async ({ id }: any) => {
     const orderItemRepo = getConnection().getRepository(OrderItem);
     const orderRepo = getConnection().getRepository(Order);
+
     const orderId = await orderItemRepo.findOne(id, { select: ["order"] });
     const order = await orderRepo.findOne(orderId);
+
     return order;
   }
 };
--- server/src/graphql/resolvers/index.ts
+++ server/src/graphql/resolvers/index.ts
@@ -1,10 +1,12 @@
 import Query from './Query';
 import Mutation from './Mutation';
 import OrderItem from './OrderItem';
+import Order from './Order';
 
 
 export default {
   Query,
   Mutation,
-  OrderItem
+  OrderItem,
+  Order
 };
