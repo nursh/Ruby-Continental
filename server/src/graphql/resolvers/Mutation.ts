474fce387c6ed84dd4e8fb10f3feabe6fef3b3f7
nursh <nsheikhmohammed@yahoo.com>
2019-01-02 02:22:13 -0500

    Implemented deleteMenuItem resolver

--- server/src/graphql/resolvers/Mutation.ts
+++ server/src/graphql/resolvers/Mutation.ts
@@ -6,27 +6,34 @@ import { OrderItem } from "../../entity/OrderItem";
 import "../../db";
 
 export default {
-  createOrder: async () => {
+  createOrder: async (): Promise<Order> => {
     const orderRepo = getConnection().getRepository(Order);
     const newOrder = orderRepo.create();
     const insertedOrder = await orderRepo.save(newOrder);
     return insertedOrder;
   },
-  deleteOrder: async (_: any, { id }: any) => {
+  deleteOrder: async (_: any, { id }: any): Promise<Boolean> => {
     const orderRepo = getConnection().getRepository(Order);
     const order = await orderRepo.delete(id);
     const { affectedRows } = order.raw;
     if (affectedRows === 1) return true;
     return false;
   },
-  createMenuItem: async (_: any, { data }: any) => {
+  createMenuItem: async (_: any, { data }: any): Promise<MenuItem> => {
     const menuRepo = getConnection().getRepository(MenuItem);
     const { name, price, category } = data;
     const newItem = menuRepo.create({ name, price, category });
     const insertedItem = await menuRepo.save(newItem);
     return insertedItem;
   },
-  createOrderItem: async (_: any, { data }: any) => {
+  deleteMenuItem: async (_: any, { id }: any): Promise<Boolean> => {
+    const menuItemRepo = getConnection().getRepository(MenuItem);
+    const menuItem = await menuItemRepo.delete(id);
+    const { affectedRows } = menuItem.raw;
+    if (affectedRows === 1) return true;
+    return false;
+  },
+  createOrderItem: async (_: any, { data }: any): Promise<OrderItem> => {
     const orderItemRepo = getConnection().getRepository(OrderItem);
     const { name, price, quantity, order } = data;
     const newOrderItem = orderItemRepo.create({ name, price, quantity, order });
