c7255426e39fc6b11926465f8dfa8bceb4b2afdb
nursh <nsheikhmohammed@yahoo.com>
2019-01-04 07:25:19 -0500

    Added utils and svg sprite for images

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 
 
+import sprite from '../img/sprite.svg';
+
+
 class Sidebar extends Component {
   render() {
     return (
       <div className="sidebar">
         <h1 className="main-title main-title--big text-center">Ruby Continental</h1>
         <div className="sidebar__categories">
+          <svg className="sidebar__icon">
+            <use xlinkHref={`${sprite}#african-hot-pot`} />
+          </svg>
         </div>
       </div>
     );
---
+++ client/src/utils/categories.js
@@ -0,0 +1,15 @@
+export default [
+  "Breakfast",
+  "Main Dish",
+  "Soups",
+  "Bites",
+  "Rice Dishes",
+  "African Hot Pot",
+  "Delights",
+  "Cold Starter",
+  "Peppered Bites",
+  "Pet Drinks",
+  "Bottle Drinks",
+  "Can Drinks",
+  "Juice"
+]
